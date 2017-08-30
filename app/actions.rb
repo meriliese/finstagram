77bb6ae5a6b6d5a88bf0431e33b853e2114f11a3
Meriliese <meriliese@gmail.com>
2017-08-30 02:57:34 +0000

    learned about active records and how to associate ActiveRecord records and relationships between data. also how to query the database. cool. its like having a friend giving you answers on the computer. also took out duplicated info which was in the index.rb which now lives in the database so we can delete that code to stay DRY. oh and i fixed my users in tux because last class i deleted users from my db which were duplicates and then when i returned that data, my user.id was nil. gross. learned how to associate user.id to 2 and 3.

--- app/actions.rb
+++ app/actions.rb
@@ -10,49 +10,7 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-    @post_Ali = {
-        username: "Ali",
-        avatar_url: "http://s3.amazonaws.com/factmag-images/wp-content/uploads/2016/06/04163548/ali.jpg",
-        photo_url: "http://img.wennermedia.com/920-width/rs-225613-13-with-muhammad-ali.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count:  0,
-        comment_count: 1,
-        comments: [{
-            username: "Ali",
-            text: "I hated every minute of training. But I said don't quit, suffer now and live the rest of your life as a champion."
-        }]
-    }    
-    
-    @post_Tyson = {
-        username: "Kid Dynamite",
-        avatar_url: "https://ombyum.files.wordpress.com/2014/03/revisiting-young-mike-tyson1537105053-jul-19-2012-600x400.jpg",
-        photo_url: "http://static.boxrec.com/thumb/c/c9/MikeTysonBerbick2.jpg/410px-MikeTysonBerbick2.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "Kid Dynamite",
-            text: "Protect yourself at all times."
-        }]
-    }
-
-    @post_Money = {
-        username: "Money May",
-        avatar_url: "http://i3.mirror.co.uk/incoming/article4926299.ece/ALTERNATES/s615/Floyd-Mayweather.jpg",
-        photo_url: "http://www.newsflash.org/2004/02/00001/0001012887.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "Money May",
-            text: "Hard work. Dedication."
-        }]
-    }
-    
-    [@post_Ali, @post_Tyson, @post_Money].to_s
-    
-    @posts = [@post_Ali, @post_Tyson, @post_Money]
-    
+    @posts = Post.order(created_at: :desc)
     erb (:index)
 end
 
