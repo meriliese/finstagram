bea407aad7783d002ada16564d36c59fe57fdbf5
Meriliese <meriliese@gmail.com>
2017-09-01 03:04:16 +0000

    W4D2: learned about validations. added a signup page for my users and added messages which tells them when there's errors in their inputs. also, added a signup.erb to my app/views.

--- app/actions.rb
+++ app/actions.rb
@@ -14,3 +14,30 @@ get '/' do
     erb (:index)
 end
 
+get '/signup' do        # if a user navigates to the path "/signup",
+    @user = User.new    # setup empty @user object
+    erb(:signup)        #render "app/views/signup.erb"
+end
+
+post '/signup' do
+    
+    # grab user input values from params
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    # instantiate a User
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    #if user validation pass and user is saved
+    if @user.save
+        "User #{username} saved!"
+    else
+        erb(:signup)
+    end
+        
+end
+
+
+
